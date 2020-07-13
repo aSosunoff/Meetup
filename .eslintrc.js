db7e65c453be61482b61ca36cc899a0b22b4d5a1
asosunoff <al.sosunoff@gmail.com>
2020-07-13 22:14:50 +0300

    first commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,20 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-  ],
-  parserOptions: {
-    parser: 'babel-eslint',
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-  },
+	root: true,
+	env: {
+		node: true,
+	},
+	extends: ['plugin:vue/essential', '@vue/airbnb'],
+	parserOptions: {
+		parser: 'babel-eslint',
+	},
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		indent: ['error', 'tab'],
+		'no-param-reassign': [2, { props: false }],
+		'arrow-parens': ['error', 'as-needed'],
+		'no-tabs': 0,
+		'object-curly-newline': 'off',
+		'operator-linebreak': ['warn', 'before'],
+	},
 };
